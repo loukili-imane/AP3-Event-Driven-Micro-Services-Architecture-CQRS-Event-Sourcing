8dd452d09edefe437d04dbf9b3dcf0162dbb76fe
loukili-imane <imane.loukili2115@gmail.com>
2023-11-20 04:06:44 +0100

    add commands

---
+++ src/main/java/ma/imane/common_api/commands/BaseCommand.java
@@ -0,0 +1,18 @@
+package ma.imane.common_api.commands;
+
+import lombok.Getter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+public abstract class BaseCommand<IDType> {
+    @TargetAggregateIdentifier
+    // the command ID is the identifier that will be used in the Aggregate
+
+    @Getter // since the commands are immutable objects no need for setters only a constructor for initializing
+    private IDType id;
+
+    public BaseCommand(IDType id) {
+        this.id = id;
+    }
+
+
+}
